08c870ea67c0c49f274acc68b68079513b009499
happylrd <happylrd17@gmail.com>
2017-07-09 15:28:57 +0800

    Init project structure
    and add logo

--- Metis/src/router/index.js
+++ Metis/src/router/index.js
@@ -1,15 +1,13 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
-
-Vue.use(Router)
-
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
-})
+// import Vue from 'vue'
+// import Router from 'vue-router'
+//
+// Vue.use(Router)
+//
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       component: Hello
+//     }
+//   ]
+// })
