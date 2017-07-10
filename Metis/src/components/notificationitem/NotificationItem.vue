fa254bb4f2120d8cac73521fb2148ec4116b5013
happylrd <happylrd17@gmail.com>
2017-07-10 15:56:50 +0800

    Add notification ui

---
+++ Metis/src/components/notificationitem/NotificationItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div style="padding: 0px 16px">
+      <md-icon class="md-primary">person</md-icon>
+
+      <md-avatar class="md-list-action">
+        <img src="../../common/image/avatar.png" alt="头像">
+      </md-avatar>
+    </div>
+
+    <div style="padding: 0px 16px;margin-top: 8px;">{{notification.content}}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      notification: {
+        type: Object
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- Metis/src/view/NotificationView.vue
+++ Metis/src/view/NotificationView.vue
@@ -1,11 +1,41 @@
 <template>
-  <div>
-    Notification
+  <div style="margin-top: 8px">
+    <div v-for="item in notifications" :key="item.id">
+      <NotificationItem :notification="item"></NotificationItem>
+      <div style="margin: 16px 0px"></div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {}
+  import NotificationItem from '../components/notificationitem/NotificationItem'
+
+  export default {
+    data () {
+      return {
+        notifications: [
+          {
+            content: '倚楼听风雨关注了你'
+          },
+          {
+            content: '倚楼听风雨2关注了你'
+          },
+          {
+            content: '倚楼听风雨3关注了你'
+          },
+          {
+            content: '倚楼听风雨4关注了你'
+          },
+          {
+            content: '倚楼听风雨5关注了你'
+          }
+        ]
+      }
+    },
+    components: {
+      NotificationItem
+    }
+  }
 </script>
 
 <style scoped>
